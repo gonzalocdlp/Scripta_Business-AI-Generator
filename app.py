55bdd5adc7c4d96cca0a3ba10f2c2adcdf40d540
gonzalocdlp <gonzalocdlp@gmail.com>
2022-08-25 16:48:40 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-import os
-import openai
-import authapi
+# import os
+# import openai
+# import authapi
 from flask import Flask, render_template, request, flash
 app = Flask(__name__, static_url_path='/static')
 app.secret_key = "RoamingGiraffe12"
@@ -11,21 +11,24 @@ def index():
     return render_template("index.html")
 @app.route("/generate", methods=["POST", "GET"])    
 def generate():
-    openai.organization = "org-SnuqljBeSr8VdBfdkdiUOGb8"
-    openai.api_key = authapi.apikey
-    openai.Model.list()
+    # openai.organization = "org-SnuqljBeSr8VdBfdkdiUOGb8"
+    # openai.api_key = authapi.apikey
+    # openai.Model.list()
     businessName=request.form['bizName']
     keywords=request.form['keywords']
     businessType=request.form['bizType']
     fullName=request.form['ownerName']
-    completion = openai.Completion.create(engine="text-curie-001", prompt=f"write a long description with a call to action about a {businessType}. Write it using the keywords {keywords}. the name of the business is {businessName} and it's owned by {fullName}", max_tokens=2)
-    flash(completion['choices'][0]['text'])
+    # completion = openai.Completion.create(engine="text-curie-001", prompt=f"write a long description with a call to action about a {businessType}. Write it using the keywords {keywords}. the name of the business is {businessName} and it's owned by {fullName}", max_tokens=2)
+    # flash(completion['choices'][0]['text'])
+    flash("Where it would go")
     return render_template("index.html")
 
 
-openai.organization = "org-SnuqljBeSr8VdBfdkdiUOGb8"
-openai.api_key = authapi.apikey
-openai.Model.list()
+# openai.organization = "org-SnuqljBeSr8VdBfdkdiUOGb8"
+# openai.api_key = authapi.apikey
+# openai.Model.list()
+
+
 # from newspaper import Article
 # from textblob import TextBlob
 # from requests import request, Request
