36146a6a4c6601b852cbaecd98597ac9bc8d228a
gonzalocdlp <gonzalocdlp@gmail.com>
2022-08-26 16:15:45 -0400

    fixed gitignore

--- app.py
+++ app.py
@@ -15,70 +15,71 @@ def index():
 
 @app.route("/generate", methods=["POST", "GET"])    
 def generate():
-    tokenAmount=80
-    openai.organization = "org-SnuqljBeSr8VdBfdkdiUOGb8"
-    openai.api_key = authapi.apikey
-    openai.Model.list()
-    businessName=request.form['bizName']
-    keywords=request.form['keywords']
-    businessType=request.form['bizType']
-    fullName=request.form['ownerName']
-    location=request.form['location']
-    service1=request.form['service1']
-    service2=request.form['service2']
-    service3=request.form['service3']
-    heading1=openai.Completion.create(engine="text-curie-001", prompt=f"generate a title for {businessType} in {location}", max_tokens=12)
-    heading2=openai.Completion.create(engine="text-curie-001", prompt=f"generate a title for an about section for {businessName} that does {businessType} in {location}", max_tokens=12)
-    heading3=openai.Completion.create(engine="text-curie-001", prompt=f"generate a title for service section for {businessType} in {location}", max_tokens=12)
-    heading4=openai.Completion.create(engine="text-curie-001", prompt=f"generate a title for why you should hire {businessName} that does {businessType} in {location}", max_tokens=12)
+    if request.method == 'POST':
+        tokenAmount=10
+        openai.organization = "org-SnuqljBeSr8VdBfdkdiUOGb8"
+        openai.api_key = authapi.apikey
+        openai.Model.list()
+        businessName=request.form['bizName']
+        keywords=request.form['keywords']
+        businessType=request.form['bizType']
+        fullName=request.form['ownerName']
+        location=request.form['location']
+        service1=request.form['service1']
+        service2=request.form['service2']
+        service3=request.form['service3']
+        heading1=openai.Completion.create(engine="text-curie-001", prompt=f"generate a title for {businessType} in {location}", max_tokens=12)
+        heading2=openai.Completion.create(engine="text-curie-001", prompt=f"generate a title for an about section for {businessName} that does {businessType} in {location}", max_tokens=12)
+        heading3=openai.Completion.create(engine="text-curie-001", prompt=f"generate a title for service section for {businessType} in {location}", max_tokens=12)
+        heading4=openai.Completion.create(engine="text-curie-001", prompt=f"generate a title for why you should hire {businessName} that does {businessType} in {location}", max_tokens=12)
 
-    heading_1=heading1['choices'][0]['text']
-    heading_2=heading2['choices'][0]['text']
-    heading_3=heading3['choices'][0]['text']
-    heading_4=heading4['choices'][0]['text']
-    bizPrompt=f"""Generate several paragraphs about a business that works with {businessType}. Write it using the keywords {keywords}. the name of the business is {businessName} and it's owned by {fullName}. it's located in {location}
-     rules:
-- no phone numbers
-- don't include personal data like proper nouns, real places, etc.
-- formal wording
--include persuasive wording  """
-    about=f"""Generate an about section about a business that works with {businessType}. the name of the business is {businessName} and it's owned by {fullName}. it's located in {location}
-     rules:
-- no phone numbers
-- don't include personal data like proper nouns, real places, etc.
-- formal wording
--include persuasive wording  """
+        heading_1=heading1['choices'][0]['text']
+        heading_2=heading2['choices'][0]['text']
+        heading_3=heading3['choices'][0]['text']
+        heading_4=heading4['choices'][0]['text']
+        bizPrompt=f"""Generate several paragraphs about a business that works with {businessType}. Write it using the keywords {keywords}. the name of the business is {businessName} and it's owned by {fullName}. it's located in {location}
+        rules:
+    - no phone numbers
+    - don't include personal data like proper nouns, real places, etc.
+    - formal wording
+    -include persuasive wording  """
+        about=f"""Generate an about section about a business that works with {businessType}. the name of the business is {businessName} and it's owned by {fullName}. it's located in {location}
+        rules:
+    - no phone numbers
+    - don't include personal data like proper nouns, real places, etc.
+    - formal wording
+    -include persuasive wording  """
 
-    service=f"""Write 1 paragraph for each of this service in {location}:
-    -{service1}
-    -{service2}
-    -{service3} 
-     rules:
-- no phone numbers
-- don't include personal data like proper nouns, real places, etc.
-- formal wording
--include persuasive wording  """
+        service=f"""Write 1 paragraph for each of this service in {location}:
+        -{service1}
+        -{service2}
+        -{service3} 
+        rules:
+    - no phone numbers
+    - don't include personal data like proper nouns, real places, etc.
+    - formal wording
+    -include persuasive wording  """
 
-    hireUs=f"""write about why you should hire my {businessType} business. the name of the business is {businessName}. it's located in {location}
-     rules:
-- no phone numbers
-- don't include personal data like proper nouns, real places, etc.
-- formal wording
--include persuasive wording  """
-    general = openai.Completion.create(engine="text-curie-001", prompt=bizPrompt, max_tokens=tokenAmount)
-    about= openai.Completion.create(engine="text-curie-001", prompt=about, max_tokens=tokenAmount)
-    service= openai.Completion.create(engine="text-curie-001", prompt=service, max_tokens=tokenAmount*2)
-    hireUs= openai.Completion.create(engine="text-curie-001", prompt=hireUs, max_tokens=tokenAmount)
-    flash(heading1['choices'][0]['text'])
-    flash(general['choices'][0]['text'])
-    flash(about['choices'][0]['text'])
-    flash(service['choices'][0]['text'])
-    flash(hireUs['choices'][0]['text'])
-    General=general['choices'][0]['text']
-    About=about['choices'][0]['text']
-    Service=service['choices'][0]['text']
-    HireUs=hireUs['choices'][0]['text']
-    return render_template("index.html")
+        hireUs=f"""write about why you should hire my {businessType} business. the name of the business is {businessName}. it's located in {location}
+        rules:
+    - no phone numbers
+    - don't include personal data like proper nouns, real places, etc.
+    - formal wording
+    -include persuasive wording  """
+        generalRes = openai.Completion.create(engine="text-curie-001", prompt=bizPrompt, max_tokens=tokenAmount)
+        aboutRes= openai.Completion.create(engine="text-curie-001", prompt=about, max_tokens=tokenAmount)
+        serviceRes= openai.Completion.create(engine="text-curie-001", prompt=service, max_tokens=tokenAmount*2)
+        hireUsRes= openai.Completion.create(engine="text-curie-001", prompt=hireUs, max_tokens=tokenAmount)
+        # flash(heading1['choices'][0]['text'])
+        # flash(general['choices'][0]['text'])
+        # flash(about['choices'][0]['text'])
+        # flash(service['choices'][0]['text'])
+        # flash(hireUs['choices'][0]['text'])
+        General=generalRes['choices'][0]['text']
+        About=aboutRes['choices'][0]['text']
+        Service=serviceRes['choices'][0]['text']
+        HireUs=hireUsRes['choices'][0]['text']
+    return render_template("index.html", General)
 
 
 # openai.organization = "org-SnuqljBeSr8VdBfdkdiUOGb8"
@@ -110,4 +111,5 @@ business="photography business in florida"
 keywords="Miami, portrait shoots, wedding shoots."
 # completion = openai.Completion.create(engine="text-curie-001", prompt=f"write a long description with a call to action about a {businessName} . Write it using the keywords {keywords} the name of the business is Miami Profile and it's owned by Gonzalo Casas", max_tokens=5)
 # print(completion['choices'][0]['text'])
-
+if __name__ == "__main__":
+    app.run(debug=True)
